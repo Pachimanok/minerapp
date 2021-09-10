268b75117cb6ee50ba28c29726199072bffc9162
pachimanok <priopelliza@gmail.com>
2021-09-10 13:30:26 -0300

    barra de home

--- resources/views/layouts/navegacion.blade.php
+++ resources/views/layouts/navegacion.blade.php
@@ -2,9 +2,26 @@
     div.sticky {
       position: sticky;
       bottom: 0;
-      background-color: yellow;
-      padding: 30px;
-      font-size: 20px;
+
     }
     </style>
-    <div class="sticky">I will stick to the screen when you reach my scroll position</div>
+    <div class="sticky">
+
+      <ul class="list-group list-group-horizontal text-center bg-white pb-2">
+        <li class="list-group-item ml-1"style="width:20%; border:none; font-size: xx-large; padding-left: 0;margin:0 0 0.5rem 1rem;padding-bottom: 0;"><a href=""><i class="fas fa-home"></i></a></li>
+        <li class="list-group-item"style="width:20%; border:none; font-size: xx-large; padding-left: 0;margin: 0 0 0.5rem 0;padding-bottom: 0;"><a href="" style="color:#80808080 !important;"><i class="fas fa-wallet"></i></a></li>
+        <li class="list-group-item" style="padding: 0; border:none;"><a href="/minado" class="btn btn-primary btn-lg" style="border-radius: 50%;
+          padding: 0.65rem 1.2rem;
+          z-index: 2;
+          border-style: solid;
+          border-width: 6px;
+          border-color: rgba(255, 255, 255, 0);
+          margin-top: -33px;
+          font-size: xx-large;
+          background: #264b8f;"><i class="fas fa-gem"></i></a></li>
+        <li class="list-group-item"style="width:20%; border:none; font-size: xx-large; padding-left: 0;margin: 0 0 0.5rem 0;padding-bottom: 0;"><a href="/minar" style="/* color:#80808080 !important; */"><i class="far fa-handshake"></i></a></li>
+        <li class="list-group-item mr-1"style="width:20%; border:none; font-size: xx-large; padding-left: 0;margin: 0 0 0.5rem 0;padding-bottom: 0;"><a href="" style="color:#80808080 !important;"><i class="far fa-bell"></i></a></li>
+      </ul>
+
+
+    </div>
