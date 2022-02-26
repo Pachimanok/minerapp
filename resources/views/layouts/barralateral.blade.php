c4c48f3a5b7f2937a45b4c9c86a42bbb2c1fb45f
pachimanok <priopelliza@gmail.com>
2022-02-26 16:17:04 -0300

    proyecto completo migracin

--- resources/views/layouts/barralateral.blade.php
+++ resources/views/layouts/barralateral.blade.php
@@ -64,7 +64,7 @@
         <a class="dropdown-item" href="{{ route('logout') }}"
             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="fas fa-sign-out-alt"></i>
-            <span>Logout</span>
+            <span>Salir</span>
         </a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
